924959922a2b6eedee9b6fe2a8fbe32218f1d8f9
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-03-10 13:28:03 -0430

    [ADD] traslate main form the csv2xml to the bin/csv2xml (NOT WORKING YET).

--- csv2xml/bin/csv2xml.py
+++ csv2xml/bin/csv2xml.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python
+import csv2xml
+import argparse
+import argcomplete
 
+
+def main():
+    f = open('__config__.py','r')
+    d = eval(f.read())
+    f.close()
+    print ' --- The script is running, please wait...'
+    for i in d.iteritems():
+        out_doc, out_data = initializate_xml_out()
+        csv_files = i[1]
+        genrate_xml_tree(csv_files, out_data, i[0])
+        aditional_parser(i[0], out_data)
+        write_xml_doc(out_doc, '../mycompany_data/data/%s.xml' % i[0] )
+    print ' --- The script successfully finish.' 
+
+if __name__ == '__main__':
+    main()
--- csv2xml/csv2xml/csv2xml.py
+++ csv2xml/csv2xml/csv2xml.py
@@ -152,16 +152,3 @@ def write_xml_doc(out_doc, xml_name):
     f.write(k)
     f.close()
 
-def main():
-    f = open('__config__.py','r')
-    d = eval(f.read())
-    f.close()
-    print ' --- The script is running, please wait...'
-    for i in d.iteritems():
-        out_doc, out_data = initializate_xml_out()
-        csv_files = i[1]
-        genrate_xml_tree(csv_files, out_data, i[0])
-        aditional_parser(i[0], out_data)
-        write_xml_doc(out_doc, '../mycompany_data/data/%s.xml' % i[0] )
-    print ' --- The script successfully finish.' 
-main()
