644f9dbcf0a9ff6f147f27c98efd26dd6c8c3465
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-06-19 18:13:46 -0430

    [ADD] convert the __config__.py generic file to a list of ordered dictionaries.

--- csv2xml/csv2xml/data/csv_template/__config__.py
+++ csv2xml/csv2xml/data/csv_template/__config__.py
@@ -1,38 +1,37 @@
-{
-'ir_sequence': ['ir_sequence_type.csv','ir_sequence.csv'],
-'account_account_type': ['account_account_type.csv'],
-'account_account': ['account_account.csv'],
-'payment_term' : ['payment_term.csv', 'account_payment_term_line.csv'],
+[
+    {'account_account_type': ['account_account_type.csv']},
+    {'account_account': ['account_account.csv']},
+    {'res_company': ['res_company.csv']},
+    {'ir_sequence': ['ir_sequence_type.csv','ir_sequence.csv']},
+    {'payment_term' : ['payment_term.csv', 'account_payment_term_line.csv']},
 
-'res_currency': ['res_currency.csv','res_currency_rate.csv'],
+    {'res_currency': ['res_currency.csv','res_currency_rate.csv']},
 
-'stock_location': ['stock_location.csv'],
-'stock_warehouse': ['stock_warehouse.csv'],
-'sale_shop': ['sale_shop.csv'],
+    {'stock_location': ['stock_location.csv']},
+    {'stock_warehouse': ['stock_warehouse.csv']},
+    {'sale_shop': ['sale_shop.csv']},
 
-'res_bank': ['res_bank.csv'],
-'account_fiscalyear': ['account_fiscalyear.csv', 'account_period.csv'],
-'account_journal': ['account_journal.csv'],
+    {'res_bank': ['res_bank.csv']},
+    {'account_fiscalyear': ['account_fiscalyear.csv', 'account_period.csv']},
+    {'account_journal': ['account_journal.csv']},
 
-'islr_wh_concept': ['islr_wh_concept.csv'],
-'account_tax': ['account_tax.csv'],
+    {'islr_wh_concept': ['islr_wh_concept.csv']},
+    {'account_tax': ['account_tax.csv']},
 
-'product_uom_categ': ['product_uom_categ.csv'],
-'product_uom': ['product_uom.csv'],
-'product_category' : ['product_category.csv'],
-'product_product' : ['product_template.csv', 'product_product.csv'],
-'product_pricelist' : ['product_pricelist.csv', 'product_pricelist_version.csv','product_pricelist_item.csv'],
+    {'product_uom_categ': ['product_uom_categ.csv']},
+    {'product_uom': ['product_uom.csv']},
+    {'product_category' : ['product_category.csv']},
+    {'product_product' : ['product_template.csv', 'product_product.csv']},
+    {'product_pricelist' : ['product_pricelist.csv', 'product_pricelist_version.csv','product_pricelist_item.csv']},
 
-'res_country_state' : ['res_country_state.csv'],
-'res_partner' : ['res_partner.csv','res_partner_bank.csv'],
-'res_company': ['res_company.csv'],
-'res_users': ['res_users.csv'],
-'res_groups': ['res_groups.csv'],
+    {'res_country_state' : ['res_country_state.csv']},
+    {'res_partner' : ['res_partner.csv','res_partner_bank.csv']},
+    {'res_users': ['res_users.csv']},
+    {'res_groups': ['res_groups.csv']},
 
-'hr': ['hr_employee_category.csv', 'hr_job.csv', 'hr_department.csv', 'hr_employee.csv', 'hr_department_2.csv', 'hr_job_2.csv'],
+    {'hr': ['hr_employee_category.csv', 'hr_job.csv', 'hr_department.csv', 'hr_employee.csv', 'hr_department_2.csv', 'hr_job_2.csv']},
 
-'stock_warehouse_orderpoint' : ['stock_warehouse_orderpoint.csv'],
-'ir_mail_server' : ['ir_mail_server.csv'],
-'fetchmail_server' : ['fetchmail_server.csv']
-
-}
+    {'stock_warehouse_orderpoint' : ['stock_warehouse_orderpoint.csv']},
+    {'ir_mail_server' : ['ir_mail_server.csv']},
+    {'fetchmail_server' : ['fetchmail_server.csv']},
+]
